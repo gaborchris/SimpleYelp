d708f0250dff43d5608da4e5af81b7f534e40832
christian <chrisgabor8@gmail.com>
2020-11-06 19:01:50 -0800

    Finished basic functionality before extensions

---
+++ app/src/main/java/edu/stanford/gaborc/simpleyelp/RestaurantAdapter.kt
@@ -0,0 +1,53 @@
+package edu.stanford.gaborc.simpleyelp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.synthetic.main.item_restaurant.view.*
+
+class RestaurantAdapter(val context: Context, val restaurants: List<YelpRestaurant>) :
+    RecyclerView.Adapter<RestaurantAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(restaurant: YelpRestaurant) {
+            if (restaurant.name.length > 18) {
+                itemView.tvName.text = "${restaurant.name.take(18)}..."
+            }
+            else {
+                itemView.tvName.text = restaurant.name
+            }
+
+            itemView.ratingBar.rating = restaurant.rating.toFloat()
+            itemView.tvNumReviews.text = "${restaurant.numReviews} reviews"
+            itemView.tvAddress.text = restaurant.location.address
+            itemView.tvCategory.text = restaurant.categories[0].title
+            itemView.tvDistance.text = restaurant.displayDistance()
+            itemView.tvPrice.text = restaurant.price
+            Glide.with(context).load(restaurant.imageUrl).apply(RequestOptions().transforms(
+                CenterCrop(), RoundedCorners(20)
+            )).into(itemView.imageView)
+
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_restaurant, parent, false))
+    }
+
+    override fun getItemCount() = restaurants.size
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val restaurant = restaurants[position]
+        holder.bind(restaurant)
+
+    }
+}
